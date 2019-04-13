a52f276137057a27ef94602e295fe63cfd80e67a
kobajanashvili <kobajanashvili@gmail.com>
2019-04-13 20:53:03 +0300

    add del from cart
    delete from cart (qty + total price)

--- models/cart.js
+++ models/cart.js
@@ -43,14 +43,14 @@ module.exports = class Cart {
       if (err) {
         return;
       }
-      const updatedCart = { ...fileContent };
+      const updatedCart = { ...JSON.parse(fileContent) };
       const product = updatedCart.products.find(prod => prod.id === id);
-      const productqty = product.qty;
+      const productQty = product.qty;
       updatedCart.products = updatedCart.products.filter(
         prod => prod.id !== id
       );
       updatedCart.totalPrice =
-        updatedCart.totalPrice - productPrice * productqty;
+        updatedCart.totalPrice - productPrice * productQty;
 
       fs.writeFile(p, JSON.stringify(updatedCart), err => {
         console.log(err);
