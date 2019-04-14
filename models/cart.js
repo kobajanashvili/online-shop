2b6f4d845b0a3c6460fb4c94defe1648e4ffd289
kobajanashvili <kobajanashvili@gmail.com>
2019-04-14 23:42:10 +0300

    add cart items

--- models/cart.js
+++ models/cart.js
@@ -61,7 +61,11 @@ module.exports = class Cart {
   static getCart(cb) {
     fs.readFile(p, (err, fileContent) => {
       const cart = JSON.parse(fileContent);
-      cb(cart);
+      if (err) {
+        cb(null);
+      } else {
+        cb(cart);
+      }
     });
   }
 };
