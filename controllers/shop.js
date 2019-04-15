92cfe98316c856a29d8ecf0c4b6065c0c855b2ed
kobajanashvili <kobajanashvili@gmail.com>
2019-04-15 23:10:13 +0300

    add input hidden for id
    add hidden id input

--- controllers/shop.js
+++ controllers/shop.js
@@ -60,6 +60,10 @@ exports.postCart = (req, res, next) => {
   res.redirect('/cart');
 };
 
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+}
+
 exports.getOrders = (req, res, next) => {
   res.render("shop/orders", {
     path: "/orders",
