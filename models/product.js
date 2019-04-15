e2ca5649209fef5a40d2d6dea01749c7422a6a73
kobajanashvili <kobajanashvili@gmail.com>
2019-04-15 23:45:05 +0300

    fix delete function

--- models/product.js
+++ models/product.js
@@ -53,7 +53,7 @@ module.exports = class Product {
         const updatedProducts = products.filter(prod => prod.id !== id);    
         fs.writeFile(p, JSON.stringify(updatedProducts), err => {
             if (!err) {
-                Cart.deleteProduct(id, product.price);
+              Cart.deleteProduct(id, product.price);
             }
         })
       });
