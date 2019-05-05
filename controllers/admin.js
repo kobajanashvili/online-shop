693fd7b51ece2f82d5d2bed7a37a8c8978d0ed48
kobajanashvili <kobajanashvili@gmail.com>
2019-05-05 16:31:34 +0300

    Update admin.js

--- controllers/admin.js
+++ controllers/admin.js
@@ -21,7 +21,7 @@ exports.postAddProduct = (req, res, next) => {
   })
     .then(result => {
       // console.log(result);
-      console.log('Created Product');
+      console.log("Created Product");
     })
     .catch(err => {
       err;
@@ -65,13 +65,15 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render("admin/products", {
-      prods: products,
-      pageTitle: "Admin Products",
-      path: "/admin/products"
-    });
-  });
+  Product.findAll()
+    .then(products => {
+      res.render("admin/products", {
+        prods: products,
+        pageTitle: "Admin Products",
+        path: "/admin/products"
+      });
+    })
+    .catch(err => console.log(err));
 };
 
 exports.postDeleteProduct = (req, res, next) => {
