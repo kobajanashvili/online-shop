e2837677204d5bcd3395053a8086631cc616760e
kobajanashvili <kobajanashvili@gmail.com>
2019-05-11 22:36:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const errorController = require("./controllers/error");
 const sequelize = require("./util/database");
 const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -22,8 +23,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
 sequelize
-  .sync()
+  .sync({force: true})
   .then(result => {
     // console.log(result);
     app.listen(3000, console.log("server start port 3000"));
