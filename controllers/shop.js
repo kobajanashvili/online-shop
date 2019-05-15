38f0edfd51d50ffc76c5ef78631fc47e0e480dcf
kobajanashvili <kobajanashvili@gmail.com>
2019-05-15 13:39:44 +0300

    Update shop.js

--- controllers/shop.js
+++ controllers/shop.js
@@ -121,9 +121,11 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart;
   req.user
     .getCart()
     .then(cart => {
+      fetchedCart = cart;
       return cart.getProducts();
     })
     .then(products => {
@@ -140,21 +142,24 @@ exports.postOrder = (req, res, next) => {
         .catch(err => console.log(err));
     })
     .then(result => {
-      res.redirect('/orders');
+      return fetchedCart.setProducts(null);
+    })
+    .then(result => {
+      res.redirect("/orders");
     })
     .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render("shop/orders", {
-    path: "/orders",
-    pageTitle: "Your Orders"
-  });
-};
-
-exports.getCheckout = (req, res, next) => {
-  res.render("shop/checkout", {
-    path: "/checkout",
-    pageTitle: "Checkout"
-  });
+  req.user
+    .getOrders({include: ['products']})
+    .then(orders => {
+      console.log(orders);
+      res.render("shop/orders", {
+        path: "/orders",
+        pageTitle: "Your Orders",
+        orders: orders
+      });
+    })
+    .catch(err => console.log(err));
 };
