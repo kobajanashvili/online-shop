f63a2855cc940eab95197f24d0798a730a86b259
kobajanashvili <kobajanashvili@gmail.com>
2019-06-13 13:39:49 +0300

    Update app.js
    connect database (atlas) with mongodb!

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
+const mongoConnect = require('./util/database');
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+// const adminRoutes = require("./routes/admin");
+// const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -24,7 +25,12 @@ app.use((req, res, next) => {
   //   .catch(err => console.log(err));
 });
 
-app.use("/admin", adminRoutes);
-app.use(shopRoutes);
+// app.use("/admin", adminRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
+
+mongoConnect((client) => {
+  console.log(client);
+  app.listen(3000);
+});
