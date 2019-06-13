7f1143cc50a1a6bc31e5b931de8e0d658f11fe3e
kobajanashvili <kobajanashvili@gmail.com>
2019-06-13 13:04:44 +0300

    Update app.js
    delete connection with siquelize

--- app.js
+++ app.js
@@ -35,39 +35,3 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
-
-Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
-
-
-
-sequelize
-  // .sync({force: true})
-  .sync()
-  .then(result => {
-    return User.findByPk(1);
-    // console.log(result);
-  })
-  .then(user => {
-    if (!user) {
-      return User.create({name: 'Koba', email: 'test@test.com'});
-    }
-    return user;
-  })
-  .then(user => {
-    // console.log(user);
-    return user.createCart();
-  })
-  .then(cart => {
-    app.listen(3000, console.log("server start port 3000"));
-  })
-  .catch(err => {
-    console.log(err);
-  });
