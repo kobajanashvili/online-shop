6606708e465483394572abdb0de165b7750e4176
kobajanashvili <kobajanashvili@gmail.com>
2019-06-23 23:50:41 +0300

    Update admin.js

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,7 +14,9 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(title, price, description, imageUrl);
-    product.save().then(result => {
+  product
+    .save()
+    .then(result => {
       // console.log(result);
       console.log("Created Product");
       res.redirect("/admin/products");
